883fda4facb6ab1a48e540e60f99a62ed1fe3e01
Rishikesh Sharma <90702705+Rishi47Sharma@users.noreply.github.com>
2022-01-22 20:02:56 +0530

    Add files via upload

---
+++ Disk_health_check_UP.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+import shutil
+import psutil
+
+def checK_disk_usage(disk):
+    du=shutil.disk_usage(disk)
+    free=du.free/du.total*100
+    return  free > 20
+def check_cpu_usage():
+    usage=psutil.cpu_precent(1)
+    return usage < 75
+
+if not checK_disk_usage("/") or not check_cpu_usage:
+    print("Error")
+else:
+    print("Everthing is OK") 
